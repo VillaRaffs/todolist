b70a0c5e4fe7705b752f01bbb59c92a5c8009c9b
Rafaela Villatore <morango20n1@gmail.com>
2025-10-07 07:58:43 -0300

    Add files via upload

---
+++ Public/script.js
@@ -0,0 +1,66 @@
+class Task{
+    constructor(description){
+        this.description = description;
+        this.completed = false;
+    }
+
+    toggleComplete(){
+        this.completed = !this.completed;
+    }
+}
+
+class TaskManager{
+    constructor(){
+        this.tasks = [];
+    }
+
+    addTask(description){
+        const task = new Task(description);
+        this.tasks.push(task);
+        this.displayTask();
+    }
+
+    removeTask(index){
+        this.tasks.splice(index, 1);
+        this.displayTask();
+    }
+
+    toggleTaskCompletion(index){
+        this.tasks[index].toggleComplete();
+        this.displayTask();
+    }
+
+    displayTask(){
+        const taskList = document.getElementById('task-list');
+        taskList.innerHTML = '';
+
+        this.tasks.forEach((task, index) => {
+            const taskItem = document.createElement('li');
+            taskItem.className = task.completed ? 'completed' : '';
+
+            const taskDescription = document.createElement('span');
+            taskDescription.textContent = taskDescription;
+            taskDescription.addEventListener('click', () => this.toggleTaskCompletion(index));
+
+            const removeButton = document.createElement('button');
+            removeButton.className ='remove-btn'
+            removeButton.textContent = 'Remove'
+            removeButton.addEventListener('click', () => this.removeTask(index));
+
+            taskItem.oppendChild(taskDescription);
+            taskItem.oppendChild(removeButton);
+            taskItem.oppendChild(taskItem);
+            
+        })
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const TaskManager = new TaskManager();
+    const addTaskBtn = document.getElementById('add-task-btn')
+    const taskInput = document.getElementById('task-input')
+
+    addTaskBtn.addEventListener('click', () => {
+        const taskDescription = taskInput.ariaValueMax.trim();
+    })
+})
