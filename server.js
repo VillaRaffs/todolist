a7b3591a7ee10e2e2eee6e4f245b26ccbdb71dcc
Rafaela Villatore <morango20n1@gmail.com>
2025-10-08 08:45:20 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -26,3 +26,36 @@ async function initDB() {
      completed INTEGER DEFAULT 0
     )`);
 }
+
+//API ENDPOINTS
+app.get('/tasks', async(req,res) => {
+    const tasks = await db.all (`SELECT * FROM tasks`);
+    res.json(tasks);
+});
+
+app.post ('/tasks', async(req,res) => {
+    const {description} = req.body;
+    const stmt = await db.prepare(`INSERT INTO tasks (description) VALUES (?)`);
+    await stmt.run(description);
+    await stmt.finalize();
+    res.status(201).json({message: 'Task added'});
+});
+
+app.delete('/tasks/:id', async(req, res) => {
+   const { id } = req.params;
+   await db.run(`DELETE FROM tasks WHERE id = ?`, id);
+   res.send(204).send();
+});
+
+app.patch('tasks/:id/tggle', async (req, res) => {
+    const { id } = req.params
+    const task = await db.get(`SELECT * FROM tasks WHERE id = ?`, id);
+    const completed = task.completed ? 0 : 1; //ToggleComplete
+    await db.run(`UPDATE tasks SET completed = ? WHERE id =?` , completed, id);
+    res.status(204).send();
+});
+
+app.listen(PORT, async() => {
+    await initDB();
+    console.log(`Server running at http://localhost:${PORT}`);
+});
